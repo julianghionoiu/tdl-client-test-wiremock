2a7783bd250eb745a4db2a502f1fe8e012fbae4a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-01-18 09:45:35 +0000

    Add console support to the Wiremock wrapper

--- wiremock-wrapper.py
+++ wiremock-wrapper.py
@@ -23,17 +23,32 @@ def run(command, port):
         download_and_show_progress(url, jar_file)
 
     if command == "start":
-        run_jar(jar_file, port, pid_file)
-        wait_until_port_is_open(port, 5)
-    elif command == "stop":
+        print "Will run and detach from CLI and return to prompt..."
+        run_jar(jar_file, port, pid_file, False)
+        wait_until_port_is_open(port, 5, 5)
+
+    if command == "status":
+        wait_until_port_is_open(port, 1, 0)
+
+    if command == "stop":
         kill_process(pid_file)
+        wait_until_port_is_closed(port, 5, 5)
+
+    if command == "console":
+        print "Entered console mode (blocking, Ctrl-C to breakout)..."
+        run_jar(jar_file, port, pid_file, True)
 
 
-def run_jar(file_name, port, pid_file):
-    proc = subprocess.Popen(["java", "-jar", file_name, "--port", str(port), "&", ])
+def run_jar(jar_path, port, pid_file, console_mode):
+    if console_mode:
+        proc = subprocess.call(["java", "-jar", jar_path, "--port", str(port) ], cwd=SCRIPT_FOLDER)
+    else:
+        proc = subprocess.Popen(["java", "-jar", jar_path, "--port", str(port), "&", ], cwd=SCRIPT_FOLDER)
+
     f = open(pid_file, "w")
     f.write(str(proc.pid))
     f.close()
+
 
 
 def download_and_show_progress(url, file_name):
@@ -59,21 +74,49 @@ def download_and_show_progress(url, file_name):
     f.close()
 
 
-def wait_until_port_is_open(port, delay):
+def wait_until_port_is_open(port, count, delay):
     n = 0
-    while n < 5:
+    while True:
         print "Is application listening on port " + str(port) + "? "
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex(('127.0.0.1', port))
         if result == 0:
             print "Yes"
             return
-        print "No. Retrying in " + str(delay) + " seconds"
+
         n = n + 1
-        time.sleep(delay)
+        if n < count:
+            print "No. Retrying in " + str(delay) + " seconds"
+            time.sleep(delay)
+        else:
+            print "No."
+            return
+
+
+def wait_until_port_is_closed(port, count, delay):
+    n = 0
+    while True:
+        print "Is application listening on port " + str(port) + "? "
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(('127.0.0.1', int(port)))
+        if result != 0:
+            print "No"
+            return
+
+        n = n + 1
+        if n < count:
+            print "Yes. Retrying in " + str(delay) + " seconds"
+            time.sleep(delay)
+        else:
+            print "Yes."
+            return
 
 
 def kill_process(pid_file):
+    if not os.path.exists(pid_file):
+        print "Already stopped."
+        return
+
     f = open(pid_file, "r")
     try:
         pid_str = f.read()
